319cfe320dd469e04a526011a48025f1b4d18d64
Gekler18 <74598919+Gekler18@users.noreply.github.com>
2022-12-04 10:44:44 +0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function add(){
+    let number = parseInt(document.getElementById("box"))
+    let result = number + 12
+    console.log(result)
+}
+document.getElementById("number").onclick = function () { alert('hello!'); };
