0e5cf36ce246d38e80e32aa371c4d46e8f5e1c6c
Shavarsh <74598919+Gekler18@users.noreply.github.com>
2023-12-24 20:46:14 -0500

    javascript.js

--- javascript.js
+++ javascript.js
@@ -1,51 +1,49 @@
-document.getElementById('result').value = 'result:'
-        function add(){
-            let number = parseFloat(document.getElementById("box").value)
-            let multipler = parseFloat(document.getElementById("multipler").value)
-            let result = number * multipler
-            document.getElementById("result").value = "result:" + result
-        document.getElementById('weatherInfo').innerText = "Weather:"
-        document.getElementById('result').value = 'result:';
-        function add() {
-            let number = parseFloat(document.getElementById("box").value);
-            let multiplier = parseFloat(document.getElementById("multipler").value);
-            let result = number * multiplier;
-            document.getElementById("result").value = "result:" + result;
-        }
-        let number = document.getElementById("number");
-        number.addEventListener("click", add)
-        let calculateButton = document.getElementById("number");
-        calculateButton.addEventListener("click", add);
-        document.addEventListener('keyup', (event) => {
-            if(event.key == "Enter"){
-                add()
-            if (event.key == "Enter") {
-                add();
-            }
+document.getElementById('result').value = 'result:';
+
+function add() {
+    let number = parseFloat(document.getElementById("box").value);
+    let multiplier = parseFloat(document.getElementById("multipler").value);
+    let result = number * multiplier;
+    document.getElementById("result").value = "result:" + result;
+}
+
+document.getElementById('weatherInfo').innerText = "Weather:";
+document.getElementById('result').value = 'result:';
+
+let number = document.getElementById("number");
+number.addEventListener("click", add);
+
+let calculateButton = document.getElementById("number");
+calculateButton.addEventListener("click", add);
+
+document.addEventListener('keyup', (event) => {
+    if (event.key == "Enter") {
+        add();
+    }
+});
+
+function getWeather() {
+    const apiKey = '8216f6e872dec38b0b49b3875a9991e2';
+    const city = document.getElementById('cityInput').value;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+        .then((response) => response.json())
+        .then((data) => {
+            const temperature = data.main.temp;
+            const description = data.weather[0].description;
+            const weatherInfo = `Temperature: ${temperature}°C, Description: ${description}`;
+
+            document.getElementById('weatherInfo').innerText = "Weather: " + weatherInfo;
+        })
+        .catch((error) => {
+            console.log('Error:', error);
+            document.getElementById('weatherInfo').innerText = 'Failed to retrieve weather data. Check your spelling.';
         });
+}
 
-        function getWeather() {
-            const apiKey = '8216f6e872dec38b0b49b3875a9991e2';
-            const city = document.getElementById('cityInput').value;
-
-            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
-                .then((response) => response.json())
-                .then((data) => {
-                    const temperature = data.main.temp;
-                    const description = data.weather[0].description;
-                    const weatherInfo = `Temperature: ${temperature}°C, Description: ${description}`;
-
-                    document.getElementById('weatherInfo').innerText = "Weather: " + weatherInfo;
-                })
-                .catch((error) => {
-                    console.log('Error:', error);
-                    document.getElementById('weatherInfo').innerText = 'Failed to retrieve weather data. Check your spelling.';
-                });
-        }
-
-        document.getElementById('getWeatherButton').addEventListener('click', getWeather);
-        document.getElementById('cityInput').addEventListener('keyup', function(event) {
-            if (event.key === 'Enter') {
-                getWeather();
-            }
-        });
+document.getElementById('getWeatherButton').addEventListener('click', getWeather);
+document.getElementById('cityInput').addEventListener('keyup', function (event) {
+    if (event.key === 'Enter') {
+        getWeather();
+    }
+});
